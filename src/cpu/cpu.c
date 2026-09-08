1c0d2f0b5dd2d4a19cc9a92821a41e888c3f7f97
remynixu <remynixu@gmail.com>
2026-09-08 22:38:12 +0800

    revamp begins! :D

---
+++ src/cpu/cpu.c
@@ -0,0 +1,1 @@
+volatile int i = 0;
--- src/main.c
+++ src/main.c
@@ -1,28 +1,3 @@
-#include "cpu.h"
-
-#include <stdio.h>
-#include <string.h>
-
-static byte ram[RAM_MAXSIZE] = {
-    0x00, 0x00, /* NOP */
-    0x30, 0x04, /* LDL */
-    0x00, 0x00, /* NOP */
-    0x10, 0x00, /* JMP */
-};
-
 int main(void){
-    struct cpu_config config;
-    {
-        config.clock_hz = 1;
-        config.ram = ram;
-        config.ram_size = RAM_MAXSIZE;
-    }
-    cpu_init(&config);
-    puts("CURRENT CPU STATE:");
-    cpu_printstate();
-    while(1){
-        cpu_cycle();
-        cpu_printstate();
-    }
     return 0;
 }
