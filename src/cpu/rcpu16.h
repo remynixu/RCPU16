1b2eac02f57b601e4319b11264603837b6c4e221
remynixu <remynixu@gmail.com>
2026-09-08 22:47:37 +0800

    hehe...

---
+++ src/cpu/rcpu16.h
@@ -0,0 +1,11 @@
+#ifndef RCPU16_H
+
+/*
+ * This header provides the functionality of an rcpu16 CPU, a dream CPU made by
+ * remynixu: https://github.com/remynixu
+ * 
+ * This CPU is inspired by 16-bit RISC with Von Neumann architecture.
+ */
+#define RCPU16_H
+
+#endif /* RCPU16_H */
--- src/cpu/sources.cmake
+++ src/cpu/sources.cmake
@@ -1,4 +1,4 @@
 target_sources(${PROJECT_NAME}
     PRIVATE
-        "${CMAKE_CURRENT_LIST_DIR}/cpu.c"
+        "${CMAKE_CURRENT_LIST_DIR}/rcpu16.c"
 )
